2f93e88c12c5e42fcd8df795451b1675aa20c8d1
Lochmoi <Lochmoimicah@gmail.com>
2023-01-25 23:05:01 +0300

    a square class commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,32 @@
 #!/usr/bin/python3
 size_t print_dlistint(const dlistint_t *h);
-def print_dlistint(h):
-    count = 0
-    current = h
-    while current:
-        print(current.n)
-        current = current.next
-        count += 1
-    return count
+class Node:
+   def __init__(self, data):
+      self.data = data
+      self.next = None
+      self.prev = None
+
+class doubly_linked_list:
+   def __init__(self):
+      self.head = None
+
+# Adding data elements		
+   def push(self, NewVal):
+      NewNode = Node(NewVal)
+      NewNode.next = self.head
+      if self.head is not None:
+         self.head.prev = NewNode
+      self.head = NewNode
+
+# Print the Doubly Linked list		
+   def listprint(self, node):
+      while (node is not None):
+         print(node.data),
+         last = node
+         node = node.next
+
+dllist = doubly_linked_list()
+dllist.push(12)
+dllist.push(8)
+dllist.push(62)
+dllist.listprint(dllist.head)
