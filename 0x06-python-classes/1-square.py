1564c07ee3a3cb4f8c4772d5055dd0adacc2e8c4
Lochmoi <Lochmoimicah@gmail.com>
2023-01-25 23:20:02 +0300

    a class square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represent the square"""
+    def __init__(self, size):
+        """initializing the square class
+        size - size of the square
+        """
+        self.__size = size
